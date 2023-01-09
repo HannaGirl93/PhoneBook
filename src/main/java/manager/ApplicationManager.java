5703eb7a51a1e6d8ffd1b39d230fadbf595d3f6c
Anna <hannagirl93@gmail.com>
2023-01-09 18:27:05 +0200

    Update ApplicationManager.java

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -1,5 +1,28 @@
 package manager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
 public class ApplicationManager {
 
+    WebDriver wd;
+    HeplerUser heplerUser;
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();//расскрытие на весь экран
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));//проверка в теч 5 секунд на отрисовку элемента
+        wd.navigate().to("https://telranedu.web.app");
+        heplerUser = new HeplerUser(wd);
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public HeplerUser getHeplerUser() {
+        return heplerUser;
+    }
 }

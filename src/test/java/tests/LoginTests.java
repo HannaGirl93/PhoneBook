9638d00d8ef8e603208af6b27c537c35c21685b0
Anna <hannagirl93@gmail.com>
2023-01-09 19:22:30 +0200

    Update LoginTests.java

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,7 +1,30 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class LoginTests extends TestBase{
 
-    
+    @Test
+    public void loginSuccess(){
 
+        app.getHeplerUser().openLoginRegistrationForm();
+        app.getHeplerUser().fillLoginRegistrationForm("bunny@mail.com", "Bunny12345$");
+        app.getHeplerUser().submitLogin();
+
+    }
+
+    @Test
+    public void loginWrongEmail(){
+
+    }
+
+    @Test
+    public void loginWrongPassword(){
+
+    }
+
+    @Test
+    public void loginUnregisterUser(){
+
+    }
 }

c5f6eceb16041f81208d97c45df5cf29b01c5550
Anna <hannagirl93@gmail.com>
2023-01-10 18:42:44 +0200

    changes

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -10,7 +10,7 @@ public class LoginTests extends TestBase{
         app.getHeplerUser().openLoginRegistrationForm();
         app.getHeplerUser().fillLoginRegistrationForm("bunny@mail.com", "Bunny12345$");
         app.getHeplerUser().submitLogin();
-
+        //logout
     }
 
     @Test
@@ -19,12 +19,15 @@ public class LoginTests extends TestBase{
         app.getHeplerUser().openLoginRegistrationForm();
         app.getHeplerUser().fillLoginRegistrationForm("bunny1@mail.com", "Bunny12345$");
         app.getHeplerUser().submitLogin();
+        //logout
 
     }
 
     @Test
     public void loginWrongEmail(){
-
+        app.getHeplerUser().openLoginRegistrationForm();
+        app.getHeplerUser().fillLoginRegistrationForm("bunny1mail.com", "Bunny12345$");
+        app.getHeplerUser().submitLogin();
     }
 
     @Test
